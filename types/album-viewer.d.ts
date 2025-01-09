986471382ad9ffcbbad4a1a832499cc040ff4ac4
DevboiDesigns <75514458+DevboiDesigns@users.noreply.github.com>
2025-01-10 00:16:19 +0700

    build

--- types/album-viewer.d.ts
+++ types/album-viewer.d.ts
@@ -5,7 +5,7 @@ import { LitElement } from "lit";
  * @slot - This element has a slot
  * @csspart button - The button
  */
-export declare class MyElement extends LitElement {
+export declare class AlbumViewer extends LitElement {
     /**
      *
      * @property url - The URL to fetch the album from
@@ -16,4 +16,9 @@ export declare class MyElement extends LitElement {
     displayAlbums(albums: any[]): void;
     render(): import("lit-html").TemplateResult<1>;
     static styles: import("lit").CSSResult;
+}
+declare global {
+    interface HTMLElementTagNameMap {
+        "album-viewer": AlbumViewer;
+    }
 }
